c171a8a5204fc55dba0ad3c261e4b94cd67f4df1
TM <t@tm.jp>
2019-11-18 18:00:12 +0900

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import DApp from './DApp';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<DApp />, document.getElementById('root'));
+function App() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">SimpleStorage</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Switch>
+          <Route exact path="/">
+            <DApp />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
