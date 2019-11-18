4dd07ed5dfc02005e7c07878fac3c0c2510db919
TM <t@tm.jp>
2019-11-18 19:47:55 +0900

    added automatical process

---
+++ gen_contract_dapp.sh
@@ -0,0 +1,14 @@
+name=$1
+mkdir app
+embark scaffold $name devicename:string sensor1:uint256 sensor2:string currentStream:ipfsImage
+mv app/*.js src/
+echo "import $name from './$name';\n$(cat src/index.js)" > src/index.js 
+
+sed -e "/<ul>/a\\
+<li><Link to=\"/$name\">$name</Link></li>" < src/index.js > tmp_index.js
+
+sed -e "/<Switch>/a\\
+<Route exact path=\"/$name\"><$name /></Route>" < tmp_index.js > src/index.js
+
+rm tmp_index.js
+rm -rf app/
